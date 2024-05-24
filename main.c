088551ae68944ab5994ff0089d71e66bfe06ca05
MiaoHao-oops <haomiao19@mails.ucas.ac.cn>
2024-05-24 13:42:34 +0800

    rootfs: remove rootfs from the project
    authored by: Miao Hao

--- main.c
+++ main.c
@@ -13,18 +13,23 @@ char child_stack[STACK_SIZE];
 
 int child(void *arg)
 {
-        char *child_args[] = {"/bin/sh", NULL};
+        char *child_args[] = {"/bin/bash", NULL};
         chdir(ROOT_PATH);
         chroot(".");
         mkdir("/proc", 0777);
         mount("proc", "/proc", "proc", 0, NULL);
-        execv("/bin/sh", child_args);
+        execv("/bin/bash", child_args);
         return 0;
 }
 
 int main()
 {
-        pid_t pid = clone(child, child_stack + STACK_SIZE, SIGCHLD | CLONE_NEWPID | CLONE_NEWNET, NULL);
+        pid_t pid;
+        int flags;
+        
+        flags = SIGCHLD | CLONE_NEWNS | CLONE_NEWUTS | CLONE_NEWPID | CLONE_NEWNET;
+        pid = clone(child, child_stack + STACK_SIZE, flags, NULL);
+        printf("child pid: %d\n", pid);
         if (pid < 0) {
                 printf("create child process failed!\n");
         }
